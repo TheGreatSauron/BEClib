833cc1d1ae6e5b0190202397dd0b87e1d0bc20d5
Saubhik Sarkar <65615451+SaubhikSarkar@users.noreply.github.com>
2022-08-18 11:34:55 -0600

    Update and rename Bose_Hubbard_RK45.h to include/Bose_Hubbard_RK45.h

--- Bose_Hubbard_RK45.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "include/Classes/Headers/RK45.h"
-#include "include/Classes/Headers/RK45_1D.h"
-#include "include/Classes/Headers/RK45_1D_Spin.h"
-#include "include/Classes/Headers/RK45_2D_Spin.h"
-
-#include "include/Functions/Example Function.h"
